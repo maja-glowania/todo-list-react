a9b2bdcb52b2e0c86f460d485972e7e913740c88
Maja Glowania <maiaglowania13@gmail.com>
2025-11-23 19:22:28 +0100

    section modificator deleted

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
-    <header className="section__header section__header--withButtons">
+    <header className="section__header">
       <h2 className="section__title">{title}</h2>
       {extraHeaderContent}
     </header>
