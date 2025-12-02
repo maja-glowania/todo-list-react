38f4fc77f721dce18e60fe38763ae38625f7f142
Maja Glowania <maiaglowania13@gmail.com>
2025-12-02 22:41:33 +0100

    refactor: moved initial state definition outside of component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import Header from "./Header";
 import Container from "./Container";
 
 function App() {
-  const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
+  const defaultTasks = [
     { id: 1, content: "przejść na React'a", done: false },
     { id: 2, content: "zjeść obiad", done: true },
-  ]);
+  ];
+
+  const [hideDone, setHideDone] = useState(false);
+
+  const [tasks, setTasks] = useState(defaultTasks);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
