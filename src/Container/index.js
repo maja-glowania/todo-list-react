4b83ebd6820c90469f85927e800c4f1e34d84322
Maja Glowania <maiaglowania13@gmail.com>
2025-12-08 23:09:11 +0100

    added styled.js to Container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,5 +1,5 @@
-import "./style.css";
+import { Main } from "./styled";
 
-const Container = ({ children }) => <main className="main">{children}</main>;
+const Container = ({ children }) => <Main>{children}</Main>;
 
 export default Container;
---
+++ src/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  max-width: 1000px;
+  padding: 20px;
+  margin: 0 auto;
+
+  @media (max-width: 767px) {
+    padding: 20px 25px;
+  }
+`;
