dc4f73a6391eccbdcd53f110dc102273e019870f
Maja Glowania <maiaglowania13@gmail.com>
2025-12-08 23:09:22 +0100

    added styled.js to Form

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,46 +1,44 @@
-.form {
+import styled from "styled-components";
+
+export const FormContainer = styled.form`
   display: grid;
   grid-template-columns: 1fr auto;
   grid-gap: 10px;
-}
 
-@media (max-width: 767px) {
-  .form {
+  @media (max-width: 767px) {
     grid-template-columns: 1fr;
     grid-gap: 10px;
   }
-}
+`;
 
-.form__input {
+export const Input = styled.input`
   height: 10px;
   width: 100%;
   max-width: 800px;
   padding: 20px;
   border: 1px solid #ddd;
-}
 
-.form__button {
+  &:focus {
+    border-color: rgb(0, 128, 128);
+    outline-color: black;
+  }
+`;
+
+export const Button = styled.button`
   background-color: rgb(0, 128, 128);
   color: white;
   border: none;
   padding: 10px;
   transition: 1s;
   width: 150px;
-}
 
-.form__button:hover {
-  background: rgb(1, 180, 180);
-  transform: scale(1.1, 1.1);
-}
+  &:hover {
+    background: rgb(1, 180, 180);
+    transform: scale(1.1, 1.1);
+  }
 
-.form__input:focus {
-  border-color: rgb(0, 128, 128);
-  outline-color: black;
-}
-
-@media (max-width: 767px) {
-  .form__button {
+  @media (max-width: 767px) {
     grid-template-columns: 1fr;
     width: 100%;
   }
-}
+`;
