72a124001bf9dcc27c7a87bde81a65c4c5021a92
Maja Glowania <maiaglowania13@gmail.com>
2025-12-13 23:37:55 +0100

    Resolve TypeError in GlobalStyle (rendering order)

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { GlobalStyle } from "./GlobalStyle";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
