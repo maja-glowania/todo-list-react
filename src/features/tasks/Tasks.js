0d026f9c606de22be7894b61c39ee10cce294562
Maja Glowania <maiaglowania13@gmail.com>
2025-12-21 14:50:34 +0100

    Move Local Storage management to Redux Saga

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,15 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setTasks } from "./tasksSlice";
+import { getTasksFromLocalStorage } from "./tasksLocalStorage";
 import Form from "./Form";
 import TasksContainer from "./TasksContainer";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import { Container } from "../../common/Container/styled";
+import Search from "./Search";
+import FetchExampleTasksButton from "./FetchExampleTasksButton";
 
 function Tasks() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const tasks = getTasksFromLocalStorage();
+    dispatch(setTasks(tasks));
+  }, [dispatch]);
+
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+        extraHeaderContent={<FetchExampleTasksButton />}
+      />
+
+      <Section title="Wyszukiwarka" body={<Search />} />
+
       <Section
         title="Lista zadań"
         body={<TasksContainer />}
